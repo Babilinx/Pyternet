dcb74dc00d9509bd42ac01d5ca04f39c53966b52
Babilinx <bastiencarretier@protonmail.com>
2022-01-30 15:00:00 +0100

    Opti:suppression des espaces/sauts à la ligne, fonctions renomées

--- Pyternet.py
+++ Pyternet.py
@@ -1,74 +1,63 @@
 #Importation des modules
 import os
-
 import time
 
 #Message de bienvenue
-print("---------------------------- \n\
-BIENVENUE SUR PYTERNET \n\
----------------------------- \n\
-Pour avoir les instruction à suivre pour créer un projt, tapez \"instru\". \n\
-Pour avoir la liste des commandes, tapez \"cmds\".")
+print("""----------------------------
+BIENVENUE SUR PYTERNET
+----------------------------
+Pour avoir les instruction à suivre pour créer un projet, tapez 'instru'.
+Pour avoir la liste des commandes, tapez 'help'.""")
+
 
 #Fonctions des commandes
-def cmds():
-	print("---------------------------- \n\
-VOICI LES COMMANDES \n\
----------------------------- \n\
-		--- Création de projet --- \n\
- - instru : Avoir les instruction pour créer un projet. \n\n\
-		--- Autres --- \n\
- - stop   : Stopper la création.")
+def help():
+	print("""\n----------------------------
+	VOICI LES COMMANDES
+----------------------------
+		\n--- Création de projet ---
+ - instru : Avoir les instruction pour créer un projet.
+		\n--- Autres ---
+ - stop   : Stopper la création.""")
 
 def instru():
-	print("---------------------------- \n\
-VOICI LES INSTRUCTIONS \n\
----------------------------- \n\
-Pour créer un projet Pyternet, tapez \"nproj\" \n\
-Si vous avez déjà créé un projet Pyternet, vous pouvez passer aux commandes !")
+	print("""\n----------------------------
+	VOICI LES INSTRUCTIONS
+----------------------------
+Pour créer un projet Pyternet, tapez 'new_project'
+Si vous avez déjà créé un projet Pyternet, vous pouvez passer aux commandes !""")
 
-def nproj():
-	in_commande_func = input("Comment s'appelle votre projet ? ")
-
-	projet_nom = in_commande_func
-
-	print(f"Nous créons votre projet avec le nom \"{projet_nom}\"...")
-
-	os.mkdir(f"{projet_nom}")
-
+def new_project():
+	project_name = input("Quel nom voulez-vous donner à votre projet ? ")
+	print(f"Nous créons votre projet avec le nom '{project_name}''...")
+	os.mkdir(f"{project_name}")
 	time.sleep(1)
-
 	print("Votre projet a été créé avec succès !")
+
 
 #Boucle de détéction des commandes
 while True:
 	#Temps d'attente
 	time.sleep(1)
-
-	#Saut de ligne
-	print()
-
 	#Demande de commande
-	in_commande = input("[Pyternet] > ")
+	cmd = input("\n[Pyternet] > ")
 
 	#Commande "cmds"
-	if in_commande == 'cmds':
-		cmds()
-
+	if cmd == 'help':
+		help()
 	#-- CREATION DE PROJET --
-
 	#Commande "instru"
-	elif in_commande == 'instru':
-		instru()
-
-	#Commande "nproj"
-	elif in_commande == 'nproj':
-		nproj()
+	elif cmd == 'instru':
+			instru()
+	#Commande "new_project"
+	elif cmd == 'new_project':
+			new_project()
 
 	#-- AUTRES --
-	elif in_commande == 'stop':
-		break
+	#Quitter la Boucle
+	elif cmd == 'exit':
+			break
 
 	#Erreur
-	else: 
-		print(f"Oups... Nous n'avons pas trouvé la commande \"{in_commande}\". Réessayez !")
+	else:
+		print(f"Oups... Nous n'avons pas trouvé la commande '{cmd}'. Réessayez !")
