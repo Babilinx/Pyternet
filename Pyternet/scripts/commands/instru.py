b00012042ba971b627d9510ea5e01680d6f20aca
BlackCoder14 <95530219+BlackCoder14@users.noreply.github.com>
2022-06-06 21:09:06 +0200

    V1

---
+++ Pyternet/scripts/commands/instru.py
@@ -0,0 +1,6 @@
+def instru():
+    print("""\n--------------------------------------------------------
+	VOICI LES INSTRUCTIONS
+--------------------------------------------------------
+Pour créer un projet Pyternet, tapez 'mkproj'
+Si vous avez déjà créé un projet Pyternet, ouvrez-le avec 'open' !""")
